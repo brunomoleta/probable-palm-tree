b5081f22b1225966580abaf5c7a78ccad92d3946
Bruno Moleta <brunomoleta@pm.me>
2023-09-05 18:49:28 -0300

    style/detailsItem

--- src/components/DetailsItem.jsx
+++ src/components/DetailsItem.jsx
@@ -1,12 +1,19 @@
+import { RiArrowDownSLine } from "react-icons/ri";
+
 /* eslint-disable react/prop-types */
 export const DetailsItem = (props) => {
   const { summary, paragraph } = props;
   return (
-    <details>
-      <summary className="heading">
-        {summary}
-      </summary>
-      <p className="paragraph">{paragraph}</p>
-    </details>
+    <>
+      <details className=" w-full text-align-left stack-large">
+
+        <summary className="flex space-between align-items-center heading">{summary}
+      <RiArrowDownSLine size={30}/>
+        
+        </summary>
+        <p className="paragraph">{paragraph}</p>
+      </details>
+      <canvas className="margin-i-end canvas"></canvas>
+    </>
   );
 };
