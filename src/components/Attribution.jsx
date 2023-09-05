8cca12ac1584da6dc50a5cc776555368da25f0af
Bruno Moleta <brunomoleta@pm.me>
2023-09-05 18:47:27 -0300

    style/footer

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -2,21 +2,16 @@ export const Attribution = () => {
   const fEM = "https://www.frontendmentor.io?ref=challenge";
   const profile = "https://www.frontendmentor.io/profile/brunomoleta";
   return (
-    <footer className="w-full grid align-items-center justify-items-center">
-      <div className="">
-        Challenge by{" "}
-        <a href="#" onClick={() => window.open(fEM, "_blank")}>
-          Frontend Mentor
-        </a>
+    <footer className="footer w-full grid align-items-center justify-items-center">
+      <p>
+
+        Challenge by <a className="hv-color-white underline" href="#" onClick={() => window.open(fEM, "_blank")}>
+          Frontend Mentor</a>
         .
-      </div>
-      <div>
-        Coded by{" "}
-        <a href="#" onClick={() => window.open(profile, "_blank")}>
-          Bruno Moleta Santos
-        </a>
+        Coded by <a className="hv-color-white underline" href="#" onClick={() => window.open(profile, "_blank")}>
+          Bruno Moleta Santos</a>
         .
-      </div>
+      </p>
     </footer>
   );
 };
