64f85db8dbc5c98b83fbddbf7f707c2c756db931
Bruno Moleta <brunomoleta@pm.me>
2023-09-06 18:32:58 -0300

    fix/map detailsItems

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,31 +1,20 @@
 import { DetailsItem } from "./DetailsItem";
+import { database } from "../database/database";
+import { nanoid } from "nanoid";
 
 export const Details = () => {
+  const newList = database.map((item) => ({ ...item, id: nanoid() }));
+
   return (
     <>
-      <DetailsItem
-        summary="How many team members can I invite?"
-        paragraph="You can invite up to 2 additional users on the Free plan. There is no limit on 
-        team members for the Premium plan."
-      />
-      <DetailsItem
-        summary="What is the maximum file upload size?"
-        paragraph="No more than 2GB. All files in your account must fit your allotted storage space."
-      />
-      <DetailsItem
-        summary="How do I reset my password?"
-        paragraph="Click “Forgot password” from the login page or “Change password” from your profile page.
-    A reset link will be emailed to you."
-      />
-      <DetailsItem
-        summary="Can I cancel my subscription?"
-        paragraph="Click “Forgot password” from the login page or “Change password” from your profile page.
-    A reset link will be emailed to you."
-      />
-      <DetailsItem
-        summary="Do you provide additional support?"
-        paragraph="Chat and email support is available 24/7. Phone lines are open during normal business hours."
-      />
+      {newList.map((item) => (
+        <DetailsItem
+          key={nanoid()}
+          item={item}
+          summary={item.summary}
+          paragraph={item.paragraph}
+        />
+      ))}
     </>
   );
 };
