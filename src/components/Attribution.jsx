10def53380223003b0853d3cba065194c9b662cd
Bruno Moleta <brunomoleta@pm.me>
2023-09-11 17:36:42 -0300

    style/footer

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -3,7 +3,7 @@ export const Attribution = () => {
   const profile = "https://www.frontendmentor.io/profile/brunomoleta";
   return (
     <footer className="footer w-full grid align-items-center justify-items-center">
-      <p>
+      <p className="text-align-center">
 
         Challenge by <a className="hv-color-white underline" href="#" onClick={() => window.open(fEM, "_blank")}>
           Frontend Mentor</a>
