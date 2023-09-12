947166bdb110a05782ca9b5cc005efd0e4d72559
Bruno Moleta <brunomoleta@pm.me>
2023-09-12 17:12:59 -0300

    refactor/component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,6 @@
 import { Illustration } from "./Illustration";
-import { Details } from "./Details";
-
 import box from "../assets/images/illustration-box-desktop.svg";
+import { Section } from "./Section";
 
 export const Container = () => {
   return (
@@ -14,16 +13,9 @@ export const Container = () => {
           alt=""
         />
 
-        <div className=" overflow grid grid-col-custom-1 align-items-center justify-items-center w-full">
+        <div className="container-max-h overflow grid grid-col-custom-1 align-items-center justify-items-center w-full">
           <Illustration classname="relative illustration" />
-          <section className="min-w-full grid section">
-            <div className="grid justify-items-center justify-items-b stack-x-large">
-              <h2 className="top-heading">FAQ</h2>
-              <div className="stack-large grid justify-items-center">
-                <Details />
-              </div>
-            </div>
-          </section>
+          <Section className="min-w-full grid section" />
         </div>
       </div>
     </main>
