6d90edc94525a01612327a3143d7cce1965216aa
Bruno Moleta <brunomoleta@pm.me>
2023-10-18 09:45:21 -0300

    refactor/classesLogic

--- src/components/DetailsItem.jsx
+++ src/components/DetailsItem.jsx
@@ -14,6 +14,11 @@ export const DetailsItem = (props) => {
 
   const { summary, paragraph, item } = props;
 
+  const summaryClass = `${summaryClasses} ${active[0] === item.id && `bold`}`;
+  const arrowClass = `color-red ${
+    active[0] === item.id ? `rotated-arrow` : `not-rotated-arrow`
+  }`;
+
   return (
     <>
       <details open={() => null} className={detailsClasses}>
@@ -25,15 +30,10 @@ export const DetailsItem = (props) => {
               ? removeId()
               : removeAddId(item.id);
           }}
-          className={`${summaryClasses} ${active[0] === item.id && `bold`}`}
+          className={summaryClass}
         >
           {summary}
-          <RiArrowDownSLine
-            className={`color-red ${
-              active[0] === item.id ? `rotated-arrow` : `not-rotated-arrow`
-            }`}
-            size={30}
-          />
+          <RiArrowDownSLine className={arrowClass} size={30} />
         </summary>
         <p className="paragraph">{paragraph}</p>
       </details>
