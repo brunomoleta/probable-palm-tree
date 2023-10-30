a96f513b1fa8eaf13a6fb1c6e49c6ca81ba2dfc9
Bruno Moleta <brunomoleta@pm.me>
2023-10-30 19:47:02 -0300

    refactor/details map through database.js

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,22 +1,21 @@
-import { DetailsItem } from "./DetailsItem";
-import { nanoid } from "nanoid";
-import { database } from "../database/database";
+import {DetailsItem} from "./DetailsItem";
+import {database} from "../database/database";
 
 
 export const Details = () => {
 
-  const newList = database.map((item, index) => ({ ...item, id: index }));
+    const newList = database.map((item, index) => ({...item, id: index}));
 
-  return (
-    <>
-      {newList.map((item) => (
-        <DetailsItem
-          key={nanoid()}
-          item={item}
-          summary={item.summary}
-          paragraph={item.paragraph}
-        />
-      ))}
-    </>
-  );
+    return (
+        <>
+            {newList.map((item) => (
+                <DetailsItem
+                    key={item.id}
+                    id={item.id}
+                    summary={item.summary}
+                    paragraph={item.paragraph}
+                />
+            ))}
+        </>
+    );
 };
