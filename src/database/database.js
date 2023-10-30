2a61b35e415e21bd0cc818d98e10146ca4ce5239
Bruno Moleta <brunomoleta@pm.me>
2023-10-30 19:47:57 -0300

    refactor/database

--- src/database/database.js
+++ src/database/database.js
@@ -1,25 +1,32 @@
+import {nanoid} from "nanoid";
+
 export const database = [
   {
+    id: nanoid(),
     summary: "How many team members can I invite?",
     paragraph:
       "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
   },
   {
+    id: nanoid(),
     summary: "What is the maximum file upload size?",
     paragraph:
       "No more than 2GB. All files in your account must fit your allotted storage space.",
   },
   {
+    id: nanoid(),
     summary: "How do I reset my password?",
     paragraph:
       "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.",
   },
   {
+    id: nanoid(),
     summary: "Can I cancel my subscription",
     paragraph:
       "Certainly, you can email us at relationship@accordeon.com and ask your cancel. No questions asked :)",
   },
   {
+    id: nanoid(),
     summary: "Do you provide additional support?",
     paragraph:
       "Chat and email support is available 24/7. Phone lines are open during normal business hours.",
